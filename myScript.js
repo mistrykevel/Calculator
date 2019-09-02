f04883c97168647849929f73ad95d63ac05a59a5
mistrykevel <mistrykevel@gmail.com>
2019-09-02 10:41:44 -0400

    fixed calculations

--- myScript.js
+++ myScript.js
@@ -1,42 +1,20 @@
-window.onload = function numbers(){
+function myFunction(){
     let a= parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
-}
-
-function myFunction(){
-    // let a= parseInt(document.getElementById("num1").value);
-    // let b = parseInt(document.getElementById("num2").value);
-    let c = document.getElementById("operator");
-    document.getElementById("ans").value = d
-    
+    let c = document.getElementById("operator").value;
+    let d = document.getElementById("ans");
 
     if (c === "+") {
-        d.html = (a + b);
+        d.value = (a + b);
     }
 
     else if(c === "-"){
-        /*d.html =*/ let d = (a - b);
+        d.value = (a - b);
     }
     else if(c === "/"){
-        /*d.html =*/ let d = (a / b);
+        d.value = (a / b);
     }
     else if(c ==="*"){
-        /*d.html =*/ let d = (a * b)
+        d.value = (a * b)
     }
 }
-
-setInterval(numbers, 100);
-
-
-// window.onload = function equal(){
-    // docuement.getElementById("equals") = d
-// }
-
-// window.onload = function numbers(){
-    // let a= parseInt(document.getElementById("num1").value);
-    // let b = parseInt(document.getElementById("num2").value);
-// }
-
-// $(function(){
-    // var d = $("equals")
-// });
